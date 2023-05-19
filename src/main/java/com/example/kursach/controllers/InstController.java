94954524b594867d5fa8fe6725b843d523a345f6
kiiiwi567 <antoha052003@gmail.com>
2023-05-19 07:51:39 +0300

    everything works as intended,only needs inst pics addition

--- src/main/java/com/example/kursach/controllers/InstController.java
+++ src/main/java/com/example/kursach/controllers/InstController.java
@@ -9,10 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.security.Principal;
-import java.util.List;
 
 
 @Controller
